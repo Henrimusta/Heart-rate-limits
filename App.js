1d9a0bc72b09558ee4eb5c36bfce82aa6a621f31
Henrimusta <mustakangas.henri@gmail.com>
2024-01-09 17:18:28 +0200

    Update App.js
    init

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState('');
+  const [hrLow, setHrLow] = useState('');
+  const [hrHigh, setHrHigh] = useState('');
+
+  const calculate = () => {
+    const highHR = (220 - age) * 0.85
+    const lowHR = (220 - age) * 0.65
+    setHrLow(lowHR.toFixed(0))
+    setHrHigh(highHR.toFixed(0))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.field}>Age</Text>
+      <TextInput
+        style={styles.field}
+        keyboardType='decimal-pad'
+        value={age}
+        onChangeText={text =>
+          setAge(text)
+        }
+      />
+      <Text style={styles.field}>HR limits</Text>
+      <Text style={styles.field}>{hrLow} - {hrHigh}</Text>
+      <Button onPress={calculate} title="Calculate" />
       <StatusBar style="auto" />
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    padding: 10,
+    backgroundColor: 'white',
     justifyContent: 'center',
+  },
+  field: {
+    marginBottom: 10,
   },
 });
